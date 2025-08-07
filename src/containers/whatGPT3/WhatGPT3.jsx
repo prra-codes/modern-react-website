bd75a120fcf7afd285cb616ebe4fa74ee85811c9
peter <peter.arth5@gmail.com>
2025-08-07 08:45:47 +0100

    done header and brand sections of website

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './whatGPT3.css';
+import Features from '../features/Features';
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section-margin" id="wgpt3">
+      <div className="gpt3__wahtgpt3-feature">
+        <Features />
+      </div>
+
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Features />
+        <Features />
+        <Features />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
